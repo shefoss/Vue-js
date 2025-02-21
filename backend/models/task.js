8319b2dcaf0fe3a5c8a92193c85c9eaeb194b17d
khalid <rabaaouikhalid@gmail.com>
2025-02-21 13:33:01 +0100

    update

--- backend/models/task.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const task = new mongoose.Schema({
-    task:String
-})
-
-const model = mongoose.model("test",task);
-
-module.exports =model;
--- backend/routers/taskRouter.js
+++
@@ -1,7 +0,0 @@
-const saveTasks = require("../controllers/taskController");
-
-const router = require("express").Router();
-
-router.post("/", saveTasks);
-
-module.exports = router;
